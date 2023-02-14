bf3e70fdb631e834dc81da8f07ec320ab1fa3935
Arhises <roma1039@gmail.com>
2023-02-14 21:26:21 +0200

    First q added

---
+++ Question_01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] FillTable(double[,] table)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.NextDouble() + rnd.Next(0, 10);
+        }
+    }
+    return table;
+}
+void PrintTable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(" " + table[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+double[,] table = new double[5, 5];
+FillTable(table);
+PrintTable(table);
